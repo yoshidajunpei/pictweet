94e3b504999f77ea9ff9d40519e1ff28203cd4c0
yoshidajunpei <111556965+yoshidajunpei@users.noreply.github.com>
2022-10-26 02:36:18 +0900

    エラー日本語表示

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -15,7 +15,13 @@ class TweetsController < ApplicationController
   end
 
   def create
-    Tweet.create(tweet_params)
+    @tweet = Tweet.new(tweet_params)
+    #バリデーションに引っかからず保存されれば、「投稿完了」の画面が呼び出される
+    if @tweet.save
+    else
+      #バリデーションに引っかかり保存されなければ、「新規投稿」の画面を呼び出す
+      render 'new'
+    end
   end
 
   def destroy
@@ -27,8 +33,13 @@ class TweetsController < ApplicationController
   end
 
   def update
-    tweet = Tweet.find(params[:id])
-    tweet.update(tweet_params)
+    @tweet = Tweet.find(params[:id])
+    #バリデーションに引っかからず更新されれば、「更新完了」の画面が呼び出される
+    if @tweet.update(tweet_params)
+    else
+      #バリデーションに引っかかり保存されなければ、「編集」の画面が呼び出される
+      render 'edit'
+    end
   end
 
   def show
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,5 @@
 class Tweet < ApplicationRecord
-  validates :text, presence: true
+  validates :text, :image, presence: true
   belongs_to :user
   has_many :comments 
-
   end
